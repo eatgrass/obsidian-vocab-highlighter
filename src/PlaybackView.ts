c2e454d0b0b2df8f03d82433aa49fe00d52f178f
Huang Jing <eatgrass@live.cn>
2023-11-09 22:31:28 +0800

    prettier

--- src/PlaybackView.ts
+++ src/PlaybackView.ts
@@ -1,34 +1,34 @@
-import { ItemView, WorkspaceLeaf } from "obsidian";
+import { ItemView, WorkspaceLeaf } from 'obsidian'
 
-import Component from "./Component.svelte";
+import Component from './Component.svelte'
 
-export const VIEW_TYPE_PLAYBACK = "example-view";
+export const VIEW_TYPE_PLAYBACK = 'example-view'
 
 export class PlaybackView extends ItemView {
-	component: Component | null = null;
+    component: Component | null = null
 
-	constructor(leaf: WorkspaceLeaf) {
-		super(leaf);
-	}
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf)
+    }
 
-	getViewType() {
-		return VIEW_TYPE_PLAYBACK;
-	}
+    getViewType() {
+        return VIEW_TYPE_PLAYBACK
+    }
 
-	getDisplayText() {
-		return "Playback";
-	}
+    getDisplayText() {
+        return 'Playback'
+    }
 
-	async onOpen() {
-		this.component = new Component({
-			target: this.contentEl,
-			props: {
-				variable: 1,
-			},
-		});
-	}
+    async onOpen() {
+        this.component = new Component({
+            target: this.contentEl,
+            props: {
+                variable: 1,
+            },
+        })
+    }
 
-	async onClose() {
-		this.component?.$destroy();
-	}
+    async onClose() {
+        this.component?.$destroy()
+    }
 }
